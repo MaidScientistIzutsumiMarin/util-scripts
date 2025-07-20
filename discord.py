ad5a252308247b8f3f41907eb816e599000226fb
MaidThatPrograms <>
2025-07-20 00:38:30 -0400

    Add the file path to each reaction edit display, fix discord normalization crash, remove commented out data

--- discord.py
+++ discord.py
@@ -18,12 +18,12 @@ from common import Common, FullyValidatedModel, get_duration, media_element
 
 @total_ordering
 class Reaction(FullyValidatedModel):
-    path: Path
+    input_path: Path
     start_time: str = ""
     output_filename_base: str = ""
 
     def __lt__(self, other: Self) -> bool:
-        return self.path < other.path
+        return self.input_path < other.input_path
 
 
 class SoundReactionsCreator(Common):
@@ -46,23 +46,26 @@ class SoundReactionsCreator(Common):
     @ui.refreshable_method
     def on_selection(self, arguments: ValueChangeEventArguments | None) -> None:
         if arguments is not None:
-            self.reactions[uuid4()] = Reaction(path=arguments.value)
+            self.reactions[uuid4()] = Reaction(input_path=arguments.value)
             self.write()  # pyright: ignore[reportCallIssue]
 
         for uuid, reaction in sorted(self.reactions.items(), key=itemgetter(1)):
             with ui.chip(removable=True, on_value_change=partial(self.on_remove_edit, uuid)).classes("h-full").props("outline"), ui.grid(columns=2):
-                media_element(reaction.path)
+                ui.label(reaction.input_path.name).classes("col-span-full text-caption text-grey")
+                media_element(reaction.input_path)
                 with ui.column():
-                    ui.input("Start Time").classes("w-full").bind_value(reaction, "start_time")
+                    ui.input("Start Time").classes("w-full").props("clearable").bind_value(reaction, "start_time")
                     ui.input("Output Filename Base").classes("w-full").bind_value(reaction, "output_filename_base")
 
     @override
     def model_post_init(self, *args: object, input_button: bool = True) -> None:
-        self._input_selector = ui.select(
-            self.serialize(self.input_paths),
-            label="Select Input",
-            on_change=lambda arguments: self.on_selection.refresh(arguments),
-        )
+        with ui.row(wrap=False).classes("w-full"):
+            self._input_selector = ui.select(
+                self.serialize(self.input_paths),
+                label="Select Input",
+                on_change=lambda arguments: self.on_selection.refresh(arguments),
+            ).classes("w-full")
+            ui.input("Output Suffix").bind_value(self, "output_suffix")
         with ui.expansion("Edits").classes("w-full"), ui.grid(columns=3):
             self.on_selection(None)
 
@@ -73,28 +76,6 @@ class SoundReactionsCreator(Common):
         super().on_input_paths_change()
         self._input_selector.set_options(self.serialize(self.input_paths))
 
-    # self.create_sound_reaction("A Ninja and an Assassin Under One Roof V1E1.mkv", "Ero manga!", "15:35")
-    # self.create_sound_reaction("Ave Mujica V1E3.mkv", "つしやあ!", "9:08")
-    # self.create_sound_reaction("Ave Mujica V1E6.mkv", "ふたりいる", "7:24")
-    # self.create_sound_reaction("BanG Dream! V1E3.mkv", "Yay! Yay!", "13:38")
-    # self.create_sound_reaction("Bocchi the Rock! V1E6.mkv", "じゃ~ん 私の「my」ベース", "10:33")
-    # self.create_sound_reaction("Girls Band Cry V1E11.mkv", "Hey! Hey! Haaaa, Haaaaaa!", "16:47")
-    # self.create_sound_reaction("mono V1E8.mkv", "ま「Nagano is big」だからね", "1:27")
-    # self.create_sound_reaction("mono V1E10.mkv", "Nice idea", "20:16")
-    # self.create_sound_reaction("mono V1E11.mkv", "Avocado cream cheese!", "18:37")
-    # self.create_sound_reaction("MyGO!!!!! V1E4.mkv", "Hehehehe", "13:00")
-    # self.create_sound_reaction("MyGO!!!!! V1E5.mkv", "おもしれー女", "3:46")
-    # self.create_sound_reaction("MyGO!!!!! V1E10.mkv", "じー...", "6:39")
-    # self.create_sound_reaction("MyGO!!!!! V1E11.mkv", "私のベッド...", "20:11")
-    # self.create_sound_reaction("MyGO!!!!! V1E12.mkv", "「It's my go!」ねえ!", "4:08")
-    # self.create_sound_reaction("MyGO!!!!! V1E12.mkv", "Yay!", "10:18")
-    # self.create_sound_reaction("MyGO!!!!!xAve Mujica Joint Live Wakaremichi no, Sono Saki e Day 2.m4a", "It's MyGO!!!!!", "2:08:49")
-    # self.create_sound_reaction("MyGO!!!!!メンバーの日常「かき氷」.webm", "あー!", 23)
-    # self.create_sound_reaction("Nyamuchi Channel #2.webm", "こんばんにゃむにゃむ~~! にゃむちです~~!", 2)
-    # self.create_sound_reaction("Po-Pi-Pa Pa-Pi-Po Pa!.mp4", "Po-Pi-Pa Pa-Pi-Po Pa!")
-    # self.create_sound_reaction("Uma Musume Pretty Derby V1E4.mkv", "「Very exciting」でした!", "20:18")
-    # self.create_sound_reaction("可憐なカミツレ.flac", "Princess Togawa Sakiko Arrives")
-
     @override
     def main(self) -> Generator[Path]:
         for reaction in self.reactions.values():
@@ -104,7 +85,7 @@ class SoundReactionsCreator(Common):
             duration = 5
             stream = (
                 ffmpeg.input(
-                    reaction.path,
+                    reaction.input_path,
                     ss=self.parse_to_float(reaction.start_time),
                 )
                 .silenceremove(
@@ -118,10 +99,10 @@ class SoundReactionsCreator(Common):
                 )
             )
 
-            seconds = self.parse_to_float(str(reaction.start_time)) or 0
-            self.encode_with_progress(stream, min(5, get_duration(reaction.path)) - seconds)
+            seconds = self.parse_to_float(reaction.start_time) or 0
+            self.encode_with_progress(stream, min(5, get_duration(reaction.input_path)) - seconds)
 
-            normalize = FFmpegNormalize(extension=self.output_suffix)
+            normalize = FFmpegNormalize(audio_codec="libopus", extension=self.output_suffix)
             normalize.add_media_file(fspath(output_path), fspath(output_path))
             normalize.run_normalization()
 
